d2e9009aab1d0dc2350ff243c29b09108c0347d6
Enzo Kiyoshi <sampaioenzo000@gmail.com>
2025-09-04 14:01:10 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "PreparandoAmbiente", // Aqui você deve alterar para o nome do seu repositorio
+    name: "CursoOrientacaoDeObjetosSwift",
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
